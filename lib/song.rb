85e686851dbeaae3a8b710ad6e08e04d771a0f1d
Alex Velasquez <jtinnesco@gmail.com>
2019-06-01 06:53:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,7 @@ class Song
     
   def self.genre_count
     genre_hash = Hash.new(0)
-    @@genres.each do |genre|
-      genre_hash[genre] += 1
+    @@genres.each do {|genre| genre_hash[genre] += 1}
     end
     genre_hash
   end
